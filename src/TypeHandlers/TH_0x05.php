b500241223de0c40ed8f551e4fb3b9c627df0d28
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-09-19 22:42:43 -0400

    Program type handler: detokenizer done (+ added tests)

--- src/TypeHandlers/TH_0x05.php
+++ src/TypeHandlers/TH_0x05.php
@@ -13,6 +13,11 @@ include_once "ITIVarTypeHandler.php";
 // Type Handler for type 0x05: Program
 class TH_0x05 implements ITIVarTypeHandler
 {
+    private static $tokens_BytesToName = [];
+    private static $tokens_NameToBytes = [];
+    private static $lengthOfLongestTokenName = 0;
+    private static $firstByteOfTwoByteTokens = [];
+
     /**
      * Tokenizer
      * @param   string  $str        The program source as a string
@@ -22,6 +27,7 @@ class TH_0x05 implements ITIVarTypeHandler
     public function makeDataFromString($str = '', array $options = [])
     {
         // TODO: tokenize.
+        return [];
     }
 
 
@@ -33,6 +39,66 @@ class TH_0x05 implements ITIVarTypeHandler
      */
     public function makeStringFromData(array $data = [], array $options = [])
     {
-        // TODO: detokenize.
+        $langIdx = (isset($options['lang']) && $options['lang'] === 'fr') ? 1 : 0;
+
+        $howManyBytes = $data[0] + ($data[1] << 8);
+        array_shift($data); array_shift($data);
+        if ($howManyBytes !== count($data))
+        {
+            echo "[Warning] Token count (" . count($data) . ") and size field (" . $howManyBytes . ") mismatch!";
+        }
+
+        $str = '';
+        for ($i = 0; $i<$howManyBytes; $i++)
+        {
+            $currentToken = $data[$i];
+            $nextToken = ($i < $howManyBytes) ? $data[$i+1] : null;
+            $bytesKey = $currentToken;
+            if (in_array($currentToken, self::$firstByteOfTwoByteTokens))
+            {
+                if ($nextToken === null)
+                {
+                    echo "[Warning] Encountered an unfinished two-byte token! Setting the second byte to 0x00\n";
+                    $nextToken = 0x00;
+                }
+                $bytesKey = $nextToken + ($currentToken << 8);
+                $i++;
+            }
+            $str .= isset(self::$tokens_BytesToName[$bytesKey]) ? self::$tokens_BytesToName[$bytesKey][$langIdx] : ' [???] ';
+        }
+
+        return $str;
+    }
+
+    public static function initTokens()
+    {
+        if (($handle = fopen(__DIR__ . '/programs_tokens.csv', 'r')) !== false)
+        {
+            fgetcsv($handle); // skip first line (header)
+            while (($tokenInfo = fgetcsv($handle)) !== false)
+            {
+                if ($tokenInfo[6] === '2')
+                {
+                    if (!in_array(hexdec($tokenInfo[7]), self::$firstByteOfTwoByteTokens))
+                    {
+                        array_push(self::$firstByteOfTwoByteTokens, hexdec($tokenInfo[7]));
+                    }
+                    $bytes = hexdec($tokenInfo[8]) + (hexdec($tokenInfo[7]) << 8);
+                } else {
+                    $bytes = hexdec($tokenInfo[7]);
+                }
+                self::$tokens_BytesToName[$bytes] = [ $tokenInfo[4], $tokenInfo[5] ]; // EN, FR
+                self::$tokens_NameToBytes[$tokenInfo[4]] = $bytes; // EN
+                self::$tokens_NameToBytes[$tokenInfo[5]] = $bytes; // FR
+                $maxLenName = max(mb_strlen($tokenInfo[4], 'UTF-8'), mb_strlen($tokenInfo[5], 'UTF-8'));
+                if ($maxLenName > self::$lengthOfLongestTokenName)
+                {
+                    self::$lengthOfLongestTokenName = $maxLenName;
+                }
+            }
+            fclose($handle);
+        }
     }
 }
+
+TH_0x05::initTokens();
