705e254554e7f7bb234fca893f8d68419025567d
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-10-04 23:27:03 -0400

    Fix missing semi-colon (how did that happen?)

--- src/TypeHandlers/TH_0x05.php
+++ src/TypeHandlers/TH_0x05.php
@@ -141,7 +141,7 @@ class TH_0x05 implements ITIVarTypeHandler
             }
             fclose($handle);
         } else {
-            throw new \Exception("Could not open the tokens csv file")
+            throw new \Exception("Could not open the tokens csv file");
         }
     }
 }
