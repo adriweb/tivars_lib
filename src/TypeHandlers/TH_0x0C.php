4b4005f53d71c6e61b70b7e0dc6637b78232bd89
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-09-24 18:37:31 +0200

    Cleaner Complex vartype handler + bugfix for empty/zero coeffs.

--- src/TypeHandlers/TH_0x0C.php
+++ src/TypeHandlers/TH_0x0C.php
@@ -13,18 +13,16 @@ include_once "ITIVarTypeHandler.php";
 // Type Handler for type 0x0C: Complex Number
 class TH_0x0C implements ITIVarTypeHandler
 {
+    const dataByteCount = 2 * TH_0x00::dataByteCount;
+
     public static function makeDataFromString($str = '', array $options = [])
     {
-        $str = str_replace(' ', '', $str);
-        $str = str_replace('+i', '+1i', $str);
-        $str = str_replace('-i', '-1i', $str);
+        $str = str_replace([' ', '+i', '-i'], ['', '+1i', '-1i'], $str);
 
-        // Handle real only, real+imag, image only.
-        $isCplx = preg_match('/^'   . TH_0x00::validPattern . '()$/i', $str, $matches)
-               || preg_match('/^'   . TH_0x00::validPattern . TH_0x00::validPattern . 'i$/i', $str, $matches)
-               || preg_match('/^()' . TH_0x00::validPattern . 'i$/i', $str, $matches);
+        $matches = [];
+        $isValid = self::checkValidStringAndGetMatches($str, $matches);
 
-        if ($isCplx !== true)
+        if (!$isValid || count($matches) !== 3)
         {
             throw new \Exception("Invalid input string. Needs to be a valid complex number (a+bi)");
         }
@@ -34,6 +32,10 @@ class TH_0x0C implements ITIVarTypeHandler
         for ($i=0; $i<2; $i++)
         {
             $coeff = $matches[$i+1];
+            if (empty($coeff))
+            {
+                $coeff = "0";
+            }
 
             $data = array_merge($data, TH_0x00::makeDataFromString($coeff));
 
@@ -50,9 +52,9 @@ class TH_0x0C implements ITIVarTypeHandler
 
     public static function makeStringFromData(array $data = [], array $options = [])
     {
-        if (count($data) !== 2 * TH_0x00::dataByteCount)
+        if (count($data) !== TH_0x0C::dataByteCount)
         {
-            throw new \Exception('Invalid data array. Needs to contain ' . 2 * TH_0x00::dataByteCount . ' bytes');
+            throw new \Exception('Invalid data array. Needs to contain ' . TH_0x0C::dataByteCount . ' bytes');
         }
 
         $coeffR = TH_0x00::makeStringFromData(array_slice($data, 0, TH_0x00::dataByteCount));
@@ -62,5 +64,26 @@ class TH_0x0C implements ITIVarTypeHandler
         $str = str_replace('+-', '-', $str);
 
         return $str;
+    }
+
+    public static function checkValidString($str = '')
+    {
+        $matches = [];
+        return self::checkValidStringAndGetMatches($str, $matches);
+    }
+
+    public static function checkValidStringAndGetMatches($str = '', &$matches)
+    {
+        if (empty($str))
+        {
+            return false;
+        }
+
+        // Handle real only, real+imag, image only.
+        $isValid = preg_match('/^'   . TH_0x00::validPattern . '()$/i', $str, $matches)
+                || preg_match('/^'   . TH_0x00::validPattern . TH_0x00::validPattern . 'i$/i', $str, $matches)
+                || preg_match('/^()' . TH_0x00::validPattern . 'i$/i', $str, $matches);
+
+        return $isValid === true;
     }
 }
