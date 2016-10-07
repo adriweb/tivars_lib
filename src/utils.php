0f73cb56e5ec0cb0f9f5aeee154b58742179afba
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-10-08 00:22:39 +0200

    Better dec2frac, handles more cases.

--- src/utils.php
+++ src/utils.php
@@ -6,35 +6,71 @@
  * License: MIT
  */
 
-function gcd($a, $b)
-{
-    return ($a % $b) ? gcd($b, $a % $b) : $b;
-}
 
-function dec2frac($num = 0.0)
+// Adapted from http://stackoverflow.com/a/32903747/378298
+function dec2frac($num = 0.0, $err = 0.001)
 {
-    $num = (float)$num;
-
-    $neg = '';
-    if ($num < 0.0)
+    if ($err <= 0.0 || $err >= 1.0)
     {
-        $neg = '-';
+        $err = 0.001;
+    }
+    
+    $sign = ( $num > 0 ) ? 1 : ( ( $num < 0 ) ? -1 : 0 );
+
+    if ($sign === -1)
+    {
         $num = abs($num);
     }
-    $base = floor($num);
-    $num -= $base;
-    if ($num == 0)
+
+    if ($sign !== 0)
     {
-        return $neg . $base;
+        // $err is the maximum relative $err; convert to absolute
+        $err *= $num;
     }
 
-    list(, $numerator) = preg_split('/\./', $num, 2);
-    $denominator = pow(10, strlen($numerator));
-    $gcd = gcd($numerator, $denominator);
+    $n = (int) floor($num);
+    $num -= $n;
 
-    $norm_numer = ($numerator / $gcd);
-    $norm_denom = ($denominator / $gcd);
+    if ($num < $err)
+    {
+        return (string)($sign * $n);
+    }
 
-    return ($base > 0) ? ($neg . ($base * $norm_denom + $norm_numer). '/' . $norm_denom)
-                       : ($neg . ($norm_numer . '/' . $norm_denom));
+    if (1 - $err < $num)
+    {
+        return (string)($sign * ($n + 1));
+    }
+
+    // The lower fraction is 0/1
+    $lower_n = 0;
+    $lower_d = 1;
+
+    // The upper fraction is 1/1
+    $upper_n = 1;
+    $upper_d = 1;
+
+    while (true)
+    {
+        // The middle fraction is ($lower_n + $upper_n) / (lower_d + $upper_d)
+        $middle_n = $lower_n + $upper_n;
+        $middle_d = $lower_d + $upper_d;
+
+        if ($middle_d * ($num + $err) < $middle_n)
+        {
+            // real + $err < middle : middle is our new upper
+            $upper_n = $middle_n;
+            $upper_d = $middle_d;
+        }
+        else if ($middle_n < ($num - $err) * $middle_d)
+        {
+            // middle < real - $err : middle is our new lower
+            $lower_n = $middle_n;
+            $lower_d = $middle_d;
+        }
+        else
+        {
+            // Middle is our best fraction
+            return (string)(($n * $middle_d + $middle_n) * $sign) . '/' . (string)$middle_d;
+        }
+    }
 }
